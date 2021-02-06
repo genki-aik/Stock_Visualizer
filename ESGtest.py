951a3372f75b264f40306f5b4fc27cc4389366fc
Genki Aikawa <ga07600@uga.edu>
2021-02-06 17:57:02 -0500

    Added ESG data retrieval

--- ESGtest.py
+++ ESGtest.py
@@ -21,7 +21,11 @@ def get_esg_data(company):
 
     #Convert to json object
     output = json.loads(response.text)
+<<<<<<< HEAD
     print(output)
+=======
+    #print(output)
+>>>>>>> f7de9dc... Added ESG data retrieval
     #print(type(output))
     #Access specific attributes
     print("OUTPUT: " + output[0]['company_name'])
