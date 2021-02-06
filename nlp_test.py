3312bdbf07db13307276440e3e8c036e177f3650
Genki Aikawa <ga07600@uga.edu>
2021-02-06 12:27:19 -0500

    Determine word density

--- nlp_test.py
+++ nlp_test.py
@@ -2,13 +2,14 @@ from nltk.corpus import twitter_samples
 from nltk.tag import pos_tag
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
+from nltk import FreqDist
 import re, string
 
 positive_tweets = twitter_samples.strings('positive_tweets.json')
 negative_tweets = twitter_samples.strings('negative_tweets.json')
 text = twitter_samples.strings('tweets.20150430-223406.json')
 tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-print(tweet_tokens[0])
+#print(tweet_tokens[0])
 
 def lemmatize_sentence(tokens):
     lemmatizer = WordNetLemmatizer()
@@ -44,11 +45,34 @@ def remove_noise(tweet_tokens, stop_words = ()):
 
         if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
             cleaned_tokens.append(token.lower())
-            print(token.lower())
+            #print(token.lower())
     return cleaned_tokens
+
+def get_all_words(cleaned_tokens_list):
+    for tokens in cleaned_tokens_list:
+        for token in tokens:
+            yield token
 
 stop_words = stopwords.words('english')
 
 print(lemmatize_sentence(tweet_tokens[0]))
-print('\n')
-print(remove_noise(tweet_tokens[0], stop_words))
+#print(remove_noise(tweet_tokens[0], stop_words))
+
+positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
+
+positive_cleaned_tokens_list = []
+negative_cleaned_tokens_list = []
+
+for tokens in positive_tweet_tokens:
+    positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+
+for tokens in negative_tweet_tokens:
+    negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+
+#print(positive_tweet_tokens[500])
+#print(positive_cleaned_tokens_list[500])
+
+all_pos_words = get_all_words(positive_cleaned_tokens_list)
+freq_dist_pos = FreqDist(all_pos_words)
+#print(freq_dist_pos.most_common(10))
