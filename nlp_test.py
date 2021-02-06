7d0dd5d69b99353743b566510bb8bee204cd1d43
Genki Aikawa <ga07600@uga.edu>
2021-02-06 12:14:47 -0500

    Remove noise from tweets

--- nlp_test.py
+++ nlp_test.py
@@ -1,11 +1,14 @@
 from nltk.corpus import twitter_samples
 from nltk.tag import pos_tag
 from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+import re, string
 
 positive_tweets = twitter_samples.strings('positive_tweets.json')
 negative_tweets = twitter_samples.strings('negative_tweets.json')
 text = twitter_samples.strings('tweets.20150430-223406.json')
 tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+print(tweet_tokens[0])
 
 def lemmatize_sentence(tokens):
     lemmatizer = WordNetLemmatizer()
@@ -20,4 +23,32 @@ def lemmatize_sentence(tokens):
         lemmatized_sentence.append(lemmatizer.lemmatize(word, pos))
     return lemmatized_sentence
 
+def remove_noise(tweet_tokens, stop_words = ()):
+
+    cleaned_tokens = []
+
+    for token, tag in pos_tag(tweet_tokens):
+        token = re.sub('http[s]?://(?:[a-zA-z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', token)
+        token = re.sub("(@[A-Za-z0-9_]+)", "", token)
+
+        if tag.startswith("NN"):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else: 
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+            print(token.lower())
+    return cleaned_tokens
+
+stop_words = stopwords.words('english')
+
 print(lemmatize_sentence(tweet_tokens[0]))
+print('\n')
+print(remove_noise(tweet_tokens[0], stop_words))
